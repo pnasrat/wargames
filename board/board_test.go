38e45e2fcd8043e60d5e60308c00daded7a5df4b
Paul Nasrat <pnasrat@gmail.com>
2016-03-12 16:35:54 -0500

    Test board gives an error out of bounds.

--- board/board_test.go
+++ board/board_test.go
@@ -2,7 +2,21 @@ package board
 
 import "testing"
 
-func TestFoo(t *testing.T) {
-  NewBoard()
-  t.Fail()
+func TestGetEmptyCell(t *testing.T) {
+	b := NewBoard()
+	v, err := b.Get(0, 0)
+	if err != nil {
+		t.Fail()
+	}
+	if v != "" {
+		t.Fail()
+	}
+}
+
+func TestGetOutOfBoundsError(t *testing.T) {
+	b := NewBoard()
+	_, err := b.Get(42, 21)
+	if err == nil {
+		t.Fail()
+	}
 }
