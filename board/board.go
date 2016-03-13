507ed67b46db856e85d9f2a1d37032036b38e999
Paul Nasrat <pnasrat@gmail.com>
2016-03-13 10:56:51 -0400

    Board - have a size and error outside that size

--- board/board.go
+++ board/board.go
@@ -1,7 +1,18 @@
 package board
 
-type Board struct{}
+import "errors"
+
+type Board struct {
+	size int
+}
+
+func (b Board) Get(x, y int) (string, error) {
+	if x >= b.size || y >= b.size {
+		return "", errors.New("Out of board bounds")
+	}
+	return "", nil
+}
 
 func NewBoard() *Board {
-  return &Board{}
+	return &Board{size: 3}
 }
